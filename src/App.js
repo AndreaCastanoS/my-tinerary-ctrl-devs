186f3860df8918f1ee049ac3cb04b787dba1cbd1
AndreaCastanoS <andreacastsalazar@gmail.com>
2022-11-04 17:23:36 -0500

    modificacion archivos generales

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import AutoToTop from "./components/AutoToTop";
 import Home1 from "./components/Home1";
+import Home2 from "./components/Home2";
 import "./index.css"
 
 function App() {
+  
   return (
     <>
     <AutoToTop/>
   <Home1/>
+  <Home2/>
   </>
   );
 }
