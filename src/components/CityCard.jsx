7f00b46f19d1a8316a384839d8a15b6ce05aeffc
AntonellaTortoza18 <antonellatortoza@gmail.com>
2022-11-05 20:05:35 -0300

    creacion de componente CityCard

---
+++ src/components/CityCard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link as NavLink } from "react-router-dom";
+export default function CityCard(props) {
+  let { img } = props;
+  let { name } = props;
+
+  return (
+    <NavLink to="/detailscities">
+    <div className="hero-section">
+      <div>
+        <div class="card">
+          <div class="card__background">
+            <img className="content" src={img} alt={name}></img>
+          </div>
+          <div class="card__content">
+            <p class="card__category">{name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    </NavLink>
+  );
+}
