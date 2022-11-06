3bd51fc66940b15daf1f7bcdc04fcd20046a67c1
AntonellaTortoza18 <antonellatortoza@gmail.com>
2022-11-06 11:40:11 -0300

    cambio de estilos en el NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,58 +3,58 @@ import { useState } from "react";
 import { Link as NavLink } from "react-router-dom";
 
 export default function NavBar() {
-    let [mostrarOcultar, setMostrarOcultar] = useState(false);
-    let [mostrar, setMostrar] = useState(false);
-    let hide = () => {
-        console.log(mostrarOcultar);
-        setMostrarOcultar(!mostrarOcultar);
-    };
-    let cualquierNombre = () => {
-        console.log(mostrar);
-        setMostrar(!mostrar);
-    };
-    return (
-        <header className="flex justify-end p-2 g-15 ">
-            <img className="logo" src="" alt="" />
-            <div>
-                <button className="bt-nav " onClick={hide}>
-                    Home
-                </button>
-                {mostrarOcultar ? (
-                    <>
-                        <div className="flex column p-absolute">
-                            <NavLink to="/cities">
-                                <button className="bt-nav-c">Cities</button>
-                            </NavLink>
-                            <NavLink to="/hotels">
-                                <button className="bt-nav-c">Hotels</button>
-                            </NavLink>
-                        </div>
-                    </>
-                ) : (
-                    <></>
-                )}
+  let [mostrarOcultar, setMostrarOcultar] = useState(false);
+  let [mostrar, setMostrar] = useState(false);
+  let hide = () => {
+    console.log(mostrarOcultar);
+    setMostrarOcultar(!mostrarOcultar);
+  };
+  let cualquierNombre = () => {
+    console.log(mostrar);
+    setMostrar(!mostrar);
+  };
+  return (
+    <header className="flex justify-end p-2 g-15 ">
+      <img className="logo" src="" alt="" />
+      <div>
+        <button className="bt-nav " onClick={hide}>
+          Home
+        </button>
+        {mostrarOcultar ? (
+          <>
+            <div className="flex column justify-center align-center p-absolute">
+              <NavLink to="/cities" className="text-decoration">
+                <h3 className="bt-nav-c">Cities</h3>
+              </NavLink>
+              <NavLink to="/hotels" className="text-decoration">
+                <h3 className="bt-nav-c">Hotels</h3>
+              </NavLink>
             </div>
-            <div>
-                <button className="bt-nav" onClick={cualquierNombre}>
-                    Ussers
-                </button>
-                {mostrar ? (
-                    <>
-                        <div className="flex column p-absolute">
-                            <NavLink to="/signup">
-                                <button className="bt-nav-c">Sign Up</button>
-                            </NavLink>
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+      <div>
+        <button className="bt-nav" onClick={cualquierNombre}>
+          Ussers
+        </button>
+        {mostrar ? (
+          <>
+            <div className="flex column justify-center align-center p-absolute">
+              <NavLink to="/signup" className="text-decoration">
+                <h3 className="bt-nav-c">Sign Up</h3>
+              </NavLink>
 
-                            <NavLink to="/login">
-                                <button className="bt-nav-c">Log In</button>
-                            </NavLink>
-                        </div>
-                    </>
-                ) : (
-                    <></>
-                )}
+              <NavLink to="/login" className="text-decoration">
+                <h3 className="bt-nav-c">Log In</h3>
+              </NavLink>
             </div>
-        </header>
-    );
+          </>
+        ) : (
+          <></>
+        )}
+      </div>
+    </header>
+  );
 }
